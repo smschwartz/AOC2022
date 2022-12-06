436fc60fda99536046a1f1efce88c89c84e49ae5
Stephanie Schwartz <stephmschwartz@gmail.com>
2022-12-05 20:11:23 -0500

    Slightly less ugly day 5

--- AOC5b.py
+++ AOC5b.py
@@ -12,20 +12,15 @@ stacklines = stacklines[0:-1]
 # create empty 'stacks'
 piles = [[] for _ in range(cols)]
 for line in stacklines:
-	# this is ugly -- go along and grab letter every 4 columns (if it exists)
-	i = 1
-	for col in range(0,cols):
-		crate = line[i:i+1]
-		i += 4
-		if crate.strip():
-			(piles[col]).append(crate)
+	crates = [line[x].strip() for x in range(1,len(line),4)]
+	for i,crate in enumerate(crates):
+		if crate:
+			piles[i].append(crate)
 
 for p in piles:
 	p.reverse()
 
-steps = directions.split('\n')
-# get rid of dumb blank line
-steps = steps[:-1]
+steps = (directions.split('\n'))[:-1]
 for step in steps:
 	# get all of the numbers out of the line
 	boxes,source,dest = list(map(int,re.findall('\d+',step)))
@@ -34,10 +29,7 @@ for step in steps:
 	# remove those from original stack
 	piles[source-1] = piles[source-1][:-boxes]
 	# add to new stack
-	for b in pile:
-		piles[dest-1].append(b)
-
-
+	piles[dest-1] += pile
 
 answer = [x.pop() for x in piles]
 print(str(functools.reduce(lambda x,y: x + y, answer)))
