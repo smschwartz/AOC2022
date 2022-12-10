ec495b32606626b85f58a070494a02cffc925857
Stephanie Schwartz <stephmschwartz@gmail.com>
2022-12-10 10:07:06 -0500

    cleaned up

--- AOC10b.py
+++ AOC10b.py
@@ -17,11 +17,10 @@ vipCycles = [(x+1)*cycles[x] for x in (range(19,len(cycles),40))]
 
 print("part a",functools.reduce(operator.add,vipCycles))
 print("part b:")
-for row,vals in enumerate([cycles[x:x+40] for x in range(0,len(cycles),40)]):
+for vals in [cycles[x:x+40] for x in range(0,len(cycles),40)]:
     screenRow = []
     for pixel,val in enumerate(vals):
-        regxVal = cycles[row*40+pixel]
-        if pixel in range(regxVal-1,regxVal+2):
+        if pixel in range(val-1,val+2):
             screenRow.append('#')
         else:
             screenRow.append('.')
